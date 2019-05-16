ba6dda99d64e8f4357eadd9c9d1d4493666dc1d8
Leesungkyun <g1plinic@gmail.com>
2019-05-16 15:03:19 +0900

    로딩컨트롤러 추가

--- src/pages/care-zone-ing/care-zone-ing.ts
+++ src/pages/care-zone-ing/care-zone-ing.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, Loading, LoadingController, AlertController } from 'ionic-angular';
 import { ImagesProvider } from '../../providers/images/images';
-import { CareZoneMissionIngPage} from '../care-zone-mission-ing/care-zone-mission-ing'
-import { CareZoneMissionStartPage} from '../care-zone-mission-start/care-zone-mission-start'
-import { CareZoneMissionDeadlineEndPage} from '../care-zone-mission-deadline-end/care-zone-mission-deadline-end'
+import { CareZoneMissionIngPage } from '../care-zone-mission-ing/care-zone-mission-ing'
+import { CareZoneMissionStartPage } from '../care-zone-mission-start/care-zone-mission-start'
+import { CareZoneMissionDeadlineEndPage } from '../care-zone-mission-deadline-end/care-zone-mission-deadline-end'
 
 /**
  * Generated class for the CareZoneIngPage page.
@@ -19,8 +19,9 @@ import { CareZoneMissionDeadlineEndPage} from '../care-zone-mission-deadline-end
 })
 export class CareZoneIngPage {
   carezoneData: any;
-
-  constructor(public platform: Platform, public nav: NavController, public navParams: NavParams, private images: ImagesProvider) {
+  loading: Loading;
+  constructor(public platform: Platform, public nav: NavController, public navParams: NavParams,
+    private images: ImagesProvider, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
     this.platform.ready().then((readySource) => {
       this.carezoneData = this.roadcareZone();
     });
@@ -32,24 +33,48 @@ export class CareZoneIngPage {
 
 
 
-  public mission_ing(){
-        this.nav.push(CareZoneMissionIngPage);
+  public mission_ing() {
+    this.nav.push(CareZoneMissionIngPage);
   }
-  public mission_start(){
-        this.nav.push(CareZoneMissionStartPage);
+  public mission_start() {
+    this.nav.push(CareZoneMissionStartPage);
   }
-  public mission_deadline_end(){
-        this.nav.push(CareZoneMissionDeadlineEndPage);
+  public mission_deadline_end() {
+    this.nav.push(CareZoneMissionDeadlineEndPage);
   }
 
 
 
 
   public roadcareZone() {
+    this.showLoading();
     this.images.carezoneRoad().subscribe(data => {
-      this.carezoneData = data;
-      //console.log(JSON.stringify(data));
+      if (data !== '') {
+        this.carezoneData = data;
+        //console.log(JSON.stringify(data));
+        this.loading.dismiss();
+      } else {
+        this.showError("이미지를 불러오지 못했습니다. 관리자에게 문의하세요.");
+      }
     });
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      message: text,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
--- src/pages/care-zone/care-zone.ts
+++ src/pages/care-zone/care-zone.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, Loading, LoadingController, AlertController } from 'ionic-angular';
 import { CareZoneIngPage } from '../care-zone-ing/care-zone-ing';
 import { ImagesProvider } from '../../providers/images/images';
-import { CareZoneMissionIngPage} from '../care-zone-mission-ing/care-zone-mission-ing'
-import { CareZoneMissionStartPage} from '../care-zone-mission-start/care-zone-mission-start'
-import { CareZoneMissionDeadlineEndPage} from '../care-zone-mission-deadline-end/care-zone-mission-deadline-end'
+import { CareZoneMissionIngPage } from '../care-zone-mission-ing/care-zone-mission-ing'
+import { CareZoneMissionStartPage } from '../care-zone-mission-start/care-zone-mission-start'
+import { CareZoneMissionDeadlineEndPage } from '../care-zone-mission-deadline-end/care-zone-mission-deadline-end'
 
 
 /**
@@ -20,43 +20,68 @@ import { CareZoneMissionDeadlineEndPage} from '../care-zone-mission-deadline-end
   templateUrl: 'care-zone.html',
 })
 export class CareZonePage {
-  carezoneData : any;
-
+  carezoneData: any;
+  loading: Loading;
   constructor(public platform: Platform, public nav: NavController,
-    public navParams: NavParams, private images: ImagesProvider) {
-    this.platform.ready().then((readySource) =>{
+    public navParams: NavParams, private images: ImagesProvider,
+    private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
+    this.platform.ready().then((readySource) => {
       this.carezoneData = this.roadcareZone();
     });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CareZonePage');
-}
+  }
 
 
 
-public carezone_ing(){
-      this.nav.push(CareZoneIngPage);
-}
-public mission_ing(){
-      this.nav.push(CareZoneMissionIngPage);
-}
-public mission_start(){
-      this.nav.push(CareZoneMissionStartPage);
-}
-public mission_deadline_end(){
-      this.nav.push(CareZoneMissionDeadlineEndPage);
-}
+  public carezone_ing() {
+    this.nav.push(CareZoneIngPage);
+  }
+  public mission_ing() {
+    this.nav.push(CareZoneMissionIngPage);
+  }
+  public mission_start() {
+    this.nav.push(CareZoneMissionStartPage);
+  }
+  public mission_deadline_end() {
+    this.nav.push(CareZoneMissionDeadlineEndPage);
+  }
 
 
 
 
   public roadcareZone() {
+    this.showLoading();
     this.images.carezoneRoad().subscribe(data => {
+      if(data !==''){
       this.carezoneData = data;
       //console.log(JSON.stringify(data));
+      this.loading.dismiss();
+    } else{
+      this.showError("이미지를 불러오지 못했습니다. 관리자에게 문의하세요.");
+    }
     });
 
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      message: text,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
