0e6ccd9f25c9f16c959cc5d0f2782cec1827ddc1
Leesungkyun <g1plinic@gmail.com>
2019-05-17 17:35:49 +0900

    2019-05-17 jungwoo

--- src/pages/success-home/success-home.ts
+++ src/pages/success-home/success-home.ts
@@ -1,5 +1,5 @@
 import { IonicPage } from 'ionic-angular';
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
 import { NavController, Platform, AlertController, ModalController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { ImagesProvider } from '../../providers/images/images';
@@ -9,6 +9,11 @@ import { CareZonePage } from '../care-zone/care-zone'
 import { SkinMeasureStartPage } from '../skin-measure-start/skin-measure-start'
 import { BluetoothLE } from '@ionic-native/bluetooth-le';
 import { TranslateService } from 'ng2-translate/ng2-translate'
+import { TabsPage } from '../tabs/tabs'
+import { CareZoneMissionIngPage} from '../care-zone-mission-ing/care-zone-mission-ing'
+import { CareZoneMissionStartPage} from '../care-zone-mission-start/care-zone-mission-start'
+import { CareZoneMissionDeadlineEndPage} from '../care-zone-mission-deadline-end/care-zone-mission-deadline-end'
+import { DOCUMENT } from '@angular/common';
 
 
 @IonicPage()
@@ -20,12 +25,26 @@ export class SuccessHomePage {
   userData: any;
   bannerData: any;
   imageUrl: any;
+  carezoneData: any;
+  careDataOBJ: any;
+  first_carezone_title: any;
+  first_carezone_body: any;
+  first_carezone__id: any;
+  second_carezone_title: any;
+  second_carezone_body: any;
+  second_carezone__id: any;
+  third_carezone_title: any;
+  third_carezone_body: any;
+  third_carezone__id: any;
   constructor(public platform: Platform, public nav: NavController, public auth: AuthService, public _kakaoCordovaSDK: KakaoCordovaSDK,
-    private alertCtrl: AlertController, private images: ImagesProvider, private modalCtrl: ModalController, public translateService : TranslateService
-    //public bluetoothle: BluetoothLE
-  ) {
+    private alertCtrl: AlertController, private images: ImagesProvider, private modalCtrl: ModalController, public translateService : TranslateService //public bluetoothle: BluetoothLE
+  , @Inject(DOCUMENT) document  ) {
     this.platform.ready().then((readySource) => {
       this.bannerData = this.roadbanner();
+      this.roadcareZone();
+      //this.first_carezone = this.careDataOBJ[0];
+      //this.second_carezone = this.careDataOBJ[1];
+      //this.third_carezone = this.careDataOBJ[2];
       //console.log('Platform ready from', readySource);
       //this.showAlert(readySource);
 
@@ -49,11 +68,19 @@ export class SuccessHomePage {
   //   )
   // }
 
+   public ionViewDidLoad(){
+     document.getElementById("view").style.display = "none";
+   }
+   public moisture_help(){
+        console.log('view');
+   document.getElementById("view").style.display = "block";
+   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SuccessHomePage');
-  }
-
+   public close(){
+     console.log('close');
+   document.getElementById("view").style.display = "none";
+   // document.getElementById("view2").style.display = "none";
+   }
 
   openBasicModal() {
     let myModal = this.modalCtrl.create(SkinMeasureStartPage);
@@ -67,14 +94,27 @@ export class SuccessHomePage {
 
 
   public roadbanner() {
-    //this.bannerData = this.auth.bannerRoad();
-
     this.images.bannerRoad().subscribe(data => {
       this.bannerData = data;
     });
-
-
   }
+
+  public roadcareZone() {
+    this.images.maincarezoneRoad().subscribe(data => {
+      this.first_carezone_title = data[0].title;
+      this.first_carezone_body = data[0].body;
+      this.first_carezone__id = data[0]._id;
+      this.second_carezone_title = data[1].title;
+      this.second_carezone_body = data[1].body;
+      this.second_carezone__id = data[1]._id;
+      this.third_carezone_title = data[2].title;
+      this.third_carezone_body = data[2].body;
+      this.third_carezone__id = data[2]._id;
+      this.carezoneData = data;
+    });
+  }
+
+
 
   public kakao_request() {
     this._kakaoCordovaSDK
@@ -116,6 +156,17 @@ export class SuccessHomePage {
     this.nav.push(CareZonePage);
   }
 
+  public mission_ing(){
+        this.nav.push(CareZoneMissionIngPage);
+  }
+  public mission_start(_id){
+    console.log(_id);
+        this.nav.push(CareZoneMissionStartPage,{_id: _id});
+  }
+  public mission_deadline_end(){
+        this.nav.push(CareZoneMissionDeadlineEndPage);
+  }
+
   // public logout(){
   //   this.auth.logout();
   // }
@@ -124,5 +175,14 @@ export class SuccessHomePage {
   //   this.userData = this.auth.getUserInfo();
   //
   // }
+
+
+
+
+  public openCareZoneTab(): void {
+    // The second tab is the one with the index = 1
+    //this.nav.push(TabsPage, { selectedTab: 1 });
+    this.nav.parent.select(1);
+  }
 
 }
