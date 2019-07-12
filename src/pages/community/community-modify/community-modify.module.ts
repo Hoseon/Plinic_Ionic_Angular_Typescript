e3f2143febcc13a50bc6622a241ac3570234bedc
Leesungkyun <g1plinic@gmail.com>
2019-07-12 16:23:34 +0900

    2019-07-12 jungwoo update

--- src/pages/community/community-modify/community-modify.module.ts
+++ src/pages/community/community-modify/community-modify.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CommunityModifyPage } from './community-modify';
 
@@ -9,5 +9,9 @@ import { CommunityModifyPage } from './community-modify';
   imports: [
     IonicPageModule.forChild(CommunityModifyPage),
   ],
+  schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+  ]
 })
 export class CommunityModifyPageModule {}
--- src/pages/community/community-modify/community-modify.ts
+++ src/pages/community/community-modify/community-modify.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams , Platform} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, ViewController } from 'ionic-angular';
 //import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
 
 /**
@@ -16,82 +16,15 @@ import { IonicPage, NavController, NavParams , Platform} from 'ionic-angular';
 })
 export class CommunityModifyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
-
-      this.platform.ready().then(() => {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public viewCtrl: ViewController) {
 
 
-
-
-
-
-
-
-        // if(this.platform.is('android')){
-        // const bannerConfig: AdMobFreeBannerConfig = {
-        //   // id: 'ca-app-pub-7820559752345148/7081946422',  //real id
-        //   id: 'ca-app-pub-3940256099942544/6300978111',   //test id
-        //  isTesting: true,
-        //  autoShow: true
-        // };
-        // this.admobFree.banner.config(bannerConfig);
-        //
-        // this.admobFree.banner.prepare()
-        //   .then(() => {
-        //     // banner Ad is ready
-        //     // if we set autoShow to false, then we will need to call the show method here
-        //   })
-        //   .catch(e => console.log(e));
-        // }
-    });
-  //  this.admobFree.banner.show();
-
-    // this.platform.registerBackButtonAction(() => {
-    //   let nav = app._appRoot._getActivePortal() || app.getActiveNav();
-    //   let activeView = nav.getActive();
-    //      this.admobFree.banner.hide();
-    //   if (activeView != null) {
-    //     if (this.nav.canGoBack()) { // CHECK IF THE USER IS IN THE ROOT PAGE.
-    //       this.nav.pop(); // IF IT'S NOT THE ROOT, POP A PAGE.
-    //     }
-    //     else if (activeView.isOverlay) {
-    //       activeView.dismiss();
-    //     }
-    //     else {
-    //       // backgroundMode.moveToBackground();
-    //       let alert = this.alertCtrl.create({
-    //         cssClass: 'push_alert_cancel',
-    //         title: "plinic",
-    //         message: "앱을 종료하시겠습니까?",
-    //         buttons: [
-    //           {
-    //             text: '취소',
-    //             role: 'cancel',
-    //             handler: () => {
-    //               console.log('취소');
-    //             }
-    //           },
-    //           {
-    //             text: '확인',
-    //             handler: () => {
-    //               console.log('확인'),
-    //                 this.platform.exitApp(); // IF IT'S THE ROOT, EXIT THE APP.
-    //             }
-    //           }]
-    //       });
-    //       alert.present();
-    //     }
-    //   }
-    // });
   }
 
-  ionViewWillEnter() {
-  //  this.admobFree.banner.hide();
+  public dissmiss() {
+    this.viewCtrl.dismiss();
   }
 
-  ionViewDidEnter() {
-
-    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommunityModifyPage');
