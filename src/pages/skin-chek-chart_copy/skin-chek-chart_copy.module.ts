f3925d4d711f1bdead91fcdb7e03509465766ac7
Leesungkyun <g1plinic@gmail.com>
2020-08-12 15:28:23 +0900

    스킨차트 개발 20200812 추호선

---
+++ src/pages/skin-chek-chart_copy/skin-chek-chart_copy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SkinChekChartCopyPage } from './skin-chek-chart_copy';
+
+@NgModule({
+  declarations: [
+    SkinChekChartCopyPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SkinChekChartCopyPage),
+  ],
+})
+export class SkinChekChartCopyPageModule {}
