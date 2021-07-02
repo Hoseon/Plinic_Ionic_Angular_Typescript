4a4a524fd6065f2d64599a7f7932285d9b254314
sungwoo <g1plinic.gmail.com>
2021-07-02 17:23:41 +0900

    20210702 성우 업데이트

--- src/providers/images/images.ts
+++ src/providers/images/images.ts
@@ -16,8 +16,8 @@ export const YOUTUBE_API_KEY: string = 'AIzaSyCQ7I3wA7JDk9QFWMA7VaSMCCakpTN5K_U'
 @Injectable()
 export class ImagesProvider {
 
-  apiURL = 'http://plinic.cafe24app.com/';
-  // apiURL = 'http://localhost:8001/';
+  // apiURL = 'http://plinic.cafe24app.com/';
+  apiURL = 'http://localhost:8001/';
   youTubeApiUrl = 'https://www.googleapis.com/youtube/v3/videos?';
   iamPortUrl = 'https://api.iamport.kr/';
   iamPortToken = '0bb7f5bc3b8fc3737a9a2eec8daeba22c45e5989'
@@ -615,6 +615,11 @@ export class ImagesProvider {
 
   getPlinicProductDevice() {
     return this.http.get(this.apiURL + 'product/getPlinicProductDevice/')
+      .map(response => response.json()); 
+  }
+
+  getPlinicProductNextDeal() {
+    return this.http.get(this.apiURL + 'product/getPlinicProductNextDeal/')
       .map(response => response.json()); 
   }
 
